378bdba7a6319f01411107306549cad7203229c1
silentNi <1183611386@qq.com>
2018-07-02 22:38:44 +0800

    增加用户表和问题表脚本，使用xml配置sql

--- src/test/java/com/newcoder/wenda/InitDataBaseTests.java
+++ src/test/java/com/newcoder/wenda/InitDataBaseTests.java
@@ -1,6 +1,8 @@
 package com.newcoder.wenda;
 
+import com.newcoder.wenda.dao.QuestionDAO;
 import com.newcoder.wenda.dao.UserDAO;
+import com.newcoder.wenda.model.Question;
 import com.newcoder.wenda.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 @RunWith(SpringRunner.class)
@@ -16,18 +20,29 @@ import java.util.Random;
 public class InitDataBaseTests {
 	@Autowired
 	UserDAO userDAO;
+	@Autowired
+	QuestionDAO questionDAO;
 
 	@Test
 	public void initDatabase() {
 		Random random = new Random();
 		for(int i=1; i<11;++i){
-			User user =new User();
-			user.setName(String.format("USER%d",i));
-			user.setPassword("");
-			user.setSalt("");
-			user.setHeadUrl(String.format("http://images.newcoder.com/head/%dt.png",random.nextInt(1000)));
-			userDAO.addUser(user);
+//			User user =new User();
+//			user.setName(String.format("USER%d",i));
+//			user.setPassword("");
+//			user.setSalt("");
+//			user.setHeadUrl(String.format("http://images.newcoder.com/head/%dt.png",random.nextInt(1000)));
+//			userDAO.addUser(user);
+
+//			Question question = new Question();
+//			question.setTitle(String.format("title{%d}",i));
+//			question.setContent("balalal");
+//			question.setCreateDate(new Date());
+//			question.setUserId(i);
+//			question.setCommentCount(11-i);
+//			questionDAO.addQuestion(question);
 		}
+
 	}
 
 }
